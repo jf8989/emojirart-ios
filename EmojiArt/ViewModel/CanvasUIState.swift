0a225d17c9ba0cdd626467dc5aed27a4aaea30dd
Juan Francisco Marcenaro Arellano <128626952+jf8989@users.noreply.github.com>
2025-08-15 12:16:16 -0500

    Remove unused CanvasUIState and SelectionViewModel classes to streamline codebase and improve maintainability.

--- EmojiArt/ViewModel/CanvasUIState.swift
+++
@@ -1,9 +0,0 @@
-// ViewModel/CanvasUIState.swift
-
-import Foundation
-import CoreGraphics
-
-final class CanvasUIState: ObservableObject {
-    @Published var pan: CGSize = .zero
-    @Published var zoom: CGFloat = 1
-}
